8ef7f50676b608ebab909aa0f322516d00d97092
Trần Anh Tú <47886966+TranAnhTu1998@users.noreply.github.com>
2023-05-28 19:01:21 +0700

    Edit baseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://lienlacdientu.vn',
-  webUrl: 'https://invivo.pmr.vn',
+  // baseUrl: 'http://lienlacdientu.vn',
+  // webUrl: 'https://invivo.pmr.vn',
+  // jitsiUrl: 'https://meet.pmr.vn/'
+
+  baseUrl: 'https://listest.stc.vn:8643',
+  webUrl: 'https://listest.stc.vn',
   jitsiUrl: 'https://meet.pmr.vn/'
 };
